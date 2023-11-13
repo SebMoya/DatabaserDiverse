79b54b033a3ea1eac2fc99c474c228be0735c528
SebMoya <142984154+SebMoya@users.noreply.github.com>
2023-11-13 14:02:03 +0100

    Add files via upload

---
+++ SQLÖvningarSebastian/SebastianIntro.sql
@@ -0,0 +1,42 @@
+--CREATE DATABASE DjurDb;
+
+--CREATE TABLE DjurTbl (
+--Id INT PRIMARY KEY,
+--Namn nvarchar(max) not null
+--);
+
+--INSERT INTO DjurTbl (Id, Namn)
+--VALUES 
+--(1, 'Hamster'), 
+--(2, 'Hund'),
+--(3, 'Katt'),
+--(4, 'Kanin');
+
+--SELECT * FROM DjurTbl
+--WHERE Namn LIKE '%a%';
+
+--DELETE FROM DjurTbl;
+
+--DROP TABLE DjurTbl;
+
+--DROP DATABASE DjurDb;
+
+--CREATE DATABASE KlassenDb;
+
+--USE KlassenDb; 
+
+--CREATE TABLE KlassenTbl (
+--Id INT PRIMARY KEY,
+--[Förnamn] nvarchar(max) not null,
+--Efternamn nvarchar(max) not null,
+--[Ålder] INT,
+--Hemort nvarchar(max) not null
+--);
+
+--INSERT INTO KlassenTbl (Id, [Förnamn], Efternamn, [Ålder], Hemort)
+--VALUES
+--(1, 'Sebastian', 'Moya', 37, 'Göteborg'),
+--(2, 'Maria', 'Edstrom', 28, 'Stockholm'),
+--(3, 'Lotta', 'Harmonen', 28, 'Mikkeli'),
+--(4, 'Albin', 'Hedberg Abrahamsson', 30, 'Göteborg'),
+--(5, 'Kevin', 'Sahlen', 33, 'Göteborg');
